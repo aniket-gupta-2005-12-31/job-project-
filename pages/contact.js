ab9fab69e7ef736b12635839c265a257ef82360f
ANIKET-GUPTA31 <aniketgupta2226@gmail.com>
2025-03-02 08:09:31 +0530

    done

---
+++ pages/contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className='p-4'>
+      <h1 className='text-2xl font-bold'>Contact Us</h1>
+      <p>If you have any questions, feel free to reach out!</p>
+      {/* यहाँ पर आप संपर्क फ़ॉर्म या अन्य जानकारी जोड़ सकते हैं */}
+    </div>
+  );
+};
+
+export default Contact; 
